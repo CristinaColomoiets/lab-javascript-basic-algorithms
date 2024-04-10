9f4e7e44c886146cd344f506ce97fc5124544cdc
CristinaColomoiets <colomoetscristina@gmail.com>
2024-04-10 23:16:19 +0200

    Finished exersise 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Iteration 1: Names and Input
 let hacker1 = 'Adrian';
 console.log(`The driver's name is ${hacker1}`);
+
 let hacker2 = 'Cristina';
 console.log(`The navigator's name is ${hacker2}`);
 
@@ -8,6 +9,7 @@ console.log(`The navigator's name is ${hacker2}`);
 let hacker1Letters = hacker1.length;
 let hacker2Letters = hacker2.length;
 let capitalLetters = hacker1.toUpperCase();
+
 if (hacker1.length < hacker2.length) {
     console.log(`It seems that the navigator has the longest name, it has ${hacker2Letters} characters.`);
 }
@@ -18,22 +20,33 @@ else if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
-for (let i = 0; i < hacker1Letters; i++) {
-    let wordSpace = "";
-    wordSpace += hacker1[i].toUpperCase() + " ";
-    console.log(wordSpace);
-}
+// 1. 
 let wordSpace = "";
-for (let i = 0; i < capitalLetters.length; i++) {
-    wordSpace = wordSpace + capitalLetters[i] + " ";
-
+for (let i = 0; i < hacker1Letters; i++) {
+    wordSpace += hacker1[i].toUpperCase() + " ";
 }
 console.log(wordSpace);
 
-let array = hacker2.split("");
-console.log(array);
-// let str = array.join();
-let wordReverse = array.reverse();
-let wordReverseString = wordReverse.toString();
-let wordReverseClean = wordReverseString.split(",", " ");
-console.log(`La palabra reversible tiene que ser ${wordReverseClean}`);
+// 2.
+let wordWithSpace = "";
+for (let i = 0; i < capitalLetters.length; i++) {
+    wordWithSpace = wordWithSpace + capitalLetters[i] + " ";
+    console.log(wordWithSpace);
+}
+
+
+// 3. 
+let hacker2_arr = hacker2.split('');
+console.log(hacker2_arr);
+
+let arrayReverse = hacker2_arr.reverse();  
+console.log('Array reversible: ', arrayReverse);  // concatenar en console.log correctamente, no vale hacer con ---> ${}
+
+let strReverse = arrayReverse.toString();
+console.log('String al reves: ', strReverse);
+
+let strReverseClean = strReverse.replaceAll(',', ''); // el metodo era replace
+
+console.log(`La variable hacker2 al revés sin comas: ${strReverseClean}`);
+
+
